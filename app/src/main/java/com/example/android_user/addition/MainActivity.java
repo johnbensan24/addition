df4ff157e5962cda0c939c05ec2e1815bd0a80fa
android-user <android-user@fv.amacclab.edu.ph>
2018-04-07 14:43:17 +0800

    exercise numba 1

---
+++ app/src/main/java/com/example/android_user/addition/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.android_user.addition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText firstnumber;
+    private EditText secondnumber;
+    private Button sum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        InitView();
+    }
+
+    private void InitView() {
+
+
+        firstnumber = findViewById(R.id.firstnumber);
+        secondnumber = findViewById(R.id.secondnumber);
+        sum = findViewById(R.id.sum);
+        sum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               int a=0,b=0,c;
+
+                a = Integer.parseInt(firstnumber.getText().toString());
+                b = Integer.parseInt(secondnumber.getText().toString());
+                c = a + b;
+
+                Toast.makeText(MainActivity.this, "" + c , Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+
+
+
+    }
+}
